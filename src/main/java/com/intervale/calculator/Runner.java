936c10c602fd15f9f261fd3ec326eaa61fc94710
Vikhliayeu, Vitali <Vitali.Vikhliayeu@wolterskluwer.com>
2018-03-20 16:51:59 +0300

    readme.md add circle CI status badge

--- src/main/java/com/intervale/calculator/Runner.java
+++ src/main/java/com/intervale/calculator/Runner.java
@@ -4,15 +4,14 @@ public class Runner {
 
 	public static void main(final String[] args) {
 
-		
 		System.out.println(Boolean.parseBoolean("false"));
 		System.out.println(Boolean.parseBoolean("true"));
 		System.out.println(Boolean.parseBoolean("False"));
 		System.out.println(Boolean.parseBoolean("True"));
 		System.out.println(Boolean.parseBoolean("yes"));
 		System.out.println(Boolean.parseBoolean(null));
-		
-		//Expression expression = new Expression("5 + (2 * (3 ^ 2)) + (((2 ^ 3) * 5) / 10)");
+
+		// Expression expression = new Expression("5 + (2 * (3 ^ 2)) + (((2 ^ 3) * 5) / 10)");
 
 	}
 
